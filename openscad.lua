d70163f232580cf44e39fe6d9d1ab96043d72f6a
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-03-05 07:15:41 -0800

    document the module using the new "doc" module

--- openscad.lua
+++ openscad.lua
@@ -1,3 +1,4 @@
+local doc = require('doc')
 local openscad = {}
 
 local function getbin(scad)
@@ -7,23 +8,34 @@ local function getbin(scad)
     return 'openscad'
 end
 
-openscad.model = function(path)
-    return {path = path, vars = {}}
-end
+openscad.model =
+    doc.desc[[Define a new OpenSCAD model]] ..
+    doc.sig[[path => model]] ..
+    doc.param[[path  An OpenSCAD file path (with a .scad extension)]] ..
+    function(path)
+        return {path = path, vars = {}}
+    end
 
-openscad.args = function(output, scad, opt)
-    local args = {'-o', output, '-m', 'lark'}
-    if scad.deps_file then
-        table.insert(args, '-d')
-        table.insert(args, scad.deps_file)
+openscad.args =
+    doc.desc[[Return command line arguments for an invokation of OpenSCAD]] ..
+    doc.sig[[(output, model, opt) => args]] ..
+    doc.param[[output    The output filename]] ..
+    doc.param[[model     The input model returned from openscad.model()]] ..
+    doc.param[[opt       Execution options]] ..
+    doc.param[[opt.vars  OpenSCAD variable assignments]] ..
+    function(output, scad, opt)
+        local args = {'-o', output, '-m', 'lark'}
+        if scad.deps_file then
+            table.insert(args, '-d')
+            table.insert(args, scad.deps_file)
+        end
+        for _, var in pairs(scad.vars or {}) do
+            table.insert(args, '-D')
+            table.insert(args, var)
+        end
+        table.insert(args, scad.path)
+        return args
     end
-    for _, var in pairs(scad.vars or {}) do
-        table.insert(args, '-D')
-        table.insert(args, var)
-    end
-    table.insert(args, scad.path)
-    return args
-end
 
 local function cmd(bin, args, opt)
     local cmd = {bin, args}
@@ -37,16 +49,30 @@ local function cmd(bin, args, opt)
     return cmd
 end
 
-openscad.exec = function(output, scad, opt)
-    local bin = getbin(scad)
-    local args = openscad.args(output, scad)
-    lark.exec(cmd(bin, args, opt))
-end
+openscad.exec =
+    doc.desc[[Analogous to lark.exec() for OpenSCAD commands]] ..
+    doc.sig[[(output, model, opt) => (output, error)]] ..
+    doc.param[[output    The output filename]] ..
+    doc.param[[model     The input model returned from openscad.model()]] ..
+    doc.param[[opt       Execution options understood by lark.exec()]] ..
+    doc.param[[opt.vars  OpenSCAD variable assignments]] ..
+    function(output, scad, opt)
+        local bin = getbin(scad)
+        local args = openscad.args(output, scad)
+        return lark.exec(cmd(bin, args, opt))
+    end
 
-openscad.start = function(output, scad, opt)
-    local bin = getbin(scad)
-    local args = openscad.args(output, scad)
-    lark.start(cmd(bin, args, opt))
-end
+openscad.start =
+    doc.desc[[Analogous to lark.start() for OpenSCAD commands]] ..
+    doc.sig[[(output, model, opt) => ()]] ..
+    doc.param[[output    The output filename]] ..
+    doc.param[[model     The input model returned from openscad.model()]] ..
+    doc.param[[opt       Execution options understood by lark.start()]] ..
+    doc.param[[opt.vars  OpenSCAD variable assignments]] ..
+    function(output, scad, opt)
+        local bin = getbin(scad)
+        local args = openscad.args(output, scad)
+        lark.start(cmd(bin, args, opt))
+    end
 
 return openscad
